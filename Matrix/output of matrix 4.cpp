02ab681fe1990942be37df2d6575f781d68ff9a9
ivanazdr12 <36411923+ivanazdr12@users.noreply.github.com>
2020-10-30 00:20:11 -0400

    Add files via upload

---
+++ Matrix/output of matrix 4.cpp
@@ -0,0 +1,115 @@
+                ================================
+                =====  MATRIX CALCULATOR  ======
+                ================================
+
+        Choose one of the following calculations:
+        1. Matrix Addition.
+        2. Matrix Subtraction.
+        3. Scalar Multiplication.
+        4. Matrix Multiplication.
+        5. Inverse of a Matrix.
+        6. Quit.
+
+Enter one option from 1-6
+1
+Enter the elements of Matrix 1: 7.0 2.5 -3.0 2.0
+Enter the elements of Matrix 2: 8.0 6.5 5.0 1.3
+
+Matrix Addition:
+15      9
+2       3.3
+
+Would you like to perform another calculation? (Y/N)
+y
+                ================================
+                =====  MATRIX CALCULATOR  ======
+                ================================
+
+        Choose one of the following calculations:
+        1. Matrix Addition.
+        2. Matrix Subtraction.
+        3. Scalar Multiplication.
+        4. Matrix Multiplication.
+        5. Inverse of a Matrix.
+        6. Quit.
+
+Enter one option from 1-6
+2
+Enter the elements of Matrix 1: 7.0 2.5 -3.0 2.0
+Enter the elements of Matrix 2: 8.0 6.5 5.0 1.3
+
+Matrix Subtraction:
+-1      -4
+-8      0.7
+
+Would you like to perform another calculation? (Y/N)
+y
+                ================================
+                =====  MATRIX CALCULATOR  ======
+                ================================
+
+        Choose one of the following calculations:
+        1. Matrix Addition.
+        2. Matrix Subtraction.
+        3. Scalar Multiplication.
+        4. Matrix Multiplication.
+        5. Inverse of a Matrix.
+        6. Quit.
+
+Enter one option from 1-6
+3
+Enter the elements of Matrix 1: 7.0 2.5 -3.0 2.0
+Enter a scalar integer: 4.5
+
+Scalar Multiplication:
+31.5    11.25
+-13.5   9
+
+Would you like to perform another calculation? (Y/N)
+y
+                ================================
+                =====  MATRIX CALCULATOR  ======
+                ================================
+
+        Choose one of the following calculations:
+        1. Matrix Addition.
+        2. Matrix Subtraction.
+        3. Scalar Multiplication.
+        4. Matrix Multiplication.
+        5. Inverse of a Matrix.
+        6. Quit.
+
+Enter one option from 1-6
+4
+Enter the elements of Matrix 1: 7.0 2.5 -3.0 2.0
+Enter the elements of Matrix 2: 8.0 6.5 5.0 1.3
+
+Matrix Multiplication:
+68.5     48.75
+-14     -16.9
+
+Would you like to perform another calculation? (Y/N)
+y
+                ================================
+                =====  MATRIX CALCULATOR  ======
+                ================================
+
+        Choose one of the following calculations:
+        1. Matrix Addition.
+        2. Matrix Subtraction.
+        3. Scalar Multiplication.
+        4. Matrix Multiplication.
+        5. Inverse of a Matrix.
+        6. Quit.
+
+Enter one option from 1-6
+5
+Enter the elements of Matrix 1: 7.0 2.5 -3.0 2.0
+
+determinant: 21.5
+
+Inverse of matrix is:
+0.093     -0.12
+0.14      0.33
+
+Would you like to perform another calculation? (Y/N)
